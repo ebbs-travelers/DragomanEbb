a81c09dc4e5e6e768dbce7397692663dfc79b45b
ebbs-travelers <134110760+ebbs-travelers@users.noreply.github.com>
2023-05-19 22:15:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function goToHomePage() {
+  window.location.href = "https://dragoman.insanejournal.com/";
+}
+function goToContactPage() {
+  window.location.href = "https://dragoman.insanejournal.com/914.html";
+}
+function taken() {
+  window.location.href = "Taken Page.html";
+}
+function goToindex(){window.location.href = "https://dragoman--ebbs-travelers.repl.co/"}
